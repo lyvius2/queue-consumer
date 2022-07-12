25768f44ba0f6dc2b7b41eba8405527fc4a2d800
walter.hwang <lyvius2@naver.com>
2022-07-13 00:01:29 +0900

    Initial Commit

---
+++ src/main/java/com/walter/queue/consumer/QueueConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.walter.queue.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walter/queue/consumer/QueueConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.queue.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
